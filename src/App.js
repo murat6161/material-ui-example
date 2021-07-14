5506fc8dd2308559f6c1514a1c60ae2eb847df9f
Murat <mrtgms3561@gmail.com>
2021-07-14 09:21:55 +0300

    Card, CardContent, CardActions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Typography , AppBar, Toolbar, Container, Button,    Card, CardActions, 
 import { PhotoCamera } from '@material-ui/icons'
 import useStyles from './styles'
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 
 
@@ -48,6 +50,42 @@ const App = () => {
 
       </Container>
     </div>
+
+    <Container className={classes.cardGrid} maxWidth="md">
+      <Grid container spacing={4}>
+
+    {cards.map((card) =>(
+
+<Grid item key={card} xs={12} sm={6} md={4}>
+<Grid className={classes.card}>
+  <CardMedia
+  className={classes.cardMedia}
+  image="https://source.unplash.com/random"
+  title="Image Title"/>
+  <CardContent className={classes.cardContent}>
+    <Typography gutterBottom variant="h5">
+      Heading
+    </Typography>
+
+    <Typography>
+      This is a media card. You can use this section to describe the content.
+    </Typography>
+  </CardContent>
+  <CardActions>
+    <Button size="small" color="primary">View</Button>
+    <Button size="small" color="primary">Edit</Button>
+  </CardActions>
+</Grid>
+</Grid>
+
+    ))}
+
+
+
+       
+      </Grid>
+    </Container>
+
   </main>
     
     </>
