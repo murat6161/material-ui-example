27ee3fe5259f4b3572f9fd26b6288604600895fb
Murat <mrtgms3561@gmail.com>
2021-07-14 09:00:26 +0300

    makeStyles, useStyles

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { Typography , AppBar, Toolbar, Container, Button,    Card, CardActions, CardContent, CardMedia, CssBaseline, Grid} from '@material-ui/core'
+import { PhotoCamera } from '@material-ui/icons'
+import useStyles from './styles'
+
+
+
+
+const App = () => {
+  const classes = useStyles()
+  return (
+    <>
+  <CssBaseline/>
+  <AppBar position="relative">
+    <Toolbar>
+      <PhotoCamera className={classes.icon}/>
+      <Typography variant="h6">
+        Photo Album
+      </Typography>
+    </Toolbar>
+
+  </AppBar>
+
+  <main>
+    <div className={classes.container}>
+      <Container maxWidth="sm" style={{marginTop: '100px'}}>
+        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+          Photo Album
+        </Typography>
+        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+          Hello everyone... This is a photo album and I am trying to make this sentence as long as so we can see how does it look like on the screen
+        </Typography>
+
+        <div className={classes.button}>
+          <Grid container spacing={2} justify="center">
+            <Grid item>
+               <Button variant="contained" color="primary">
+                 See my photos
+               </Button>
+            </Grid>
+
+            <Grid item>
+               <Button variant="outlined" color="primary">
+                 Secondary action
+               </Button>
+            </Grid>
+          </Grid>
+        </div>
+
+      </Container>
+    </div>
+  </main>
+    
+    </>
+  );
+}
+
+export default App;
